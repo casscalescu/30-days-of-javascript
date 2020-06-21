4f8ef976beffba4d3630673fe0d46ec3c020665b
Cassandra Calescu <casscalescu@gmail.com>
2020-06-21 18:18:22 +1000

    scoped css variables final

--- update-css-variables/app.js
+++ update-css-variables/app.js
@@ -4,7 +4,6 @@ function handleUpdate() {
   // this.dataset is an object that contains all of the data attributes from that specific element
   const suffix = this.dataset.sizing || ''; // color has no suffix and will append undefined if you don't add || ''
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
