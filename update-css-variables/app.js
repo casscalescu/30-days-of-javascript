742cedf4dda08002de31014a11b7c3e2e2f856c7
Cassandra Calescu <casscalescu@gmail.com>
2020-06-21 18:13:24 +1000

    updating css variables with js

---
+++ update-css-variables/app.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  // this.dataset is an object that contains all of the data attributes from that specific element
+  const suffix = this.dataset.sizing || ''; // color has no suffix and will append undefined if you don't add || ''
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
