a9a527de5cb7a1bd829575ae7c521528442a4703
Cassandra Calescu <casscalescu@gmail.com>
2020-06-23 19:56:06 +1000

    sorting arrays with js

---
+++ array-cardio-1/app.js
@@ -0,0 +1,73 @@
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+];
+
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson Leana', 'Bent, Silas', 'Bentsen, Llyod', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+// Array.prototype.filter()
+// 1. Filter the list of inventors for those who were born in the 1500's
+const fifteen = inventors.filter(inventor => inventor.year < 1600 && inventor.year >= 1500);
+
+// Array.prototype.map() - map always returns the same amount that you give it
+// 2. Give us an array of the inventors first and last name
+const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+
+// Array.prototype.sort()
+// 3. Sort the inventors by birthday, oldest to youngest
+const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+// const ordered = inventors.sort((a, b) => {
+//   if(a.year > b.year) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
+
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live?
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0); // 0 is where total starts looping from
+
+// 5. Sort the inventors by years lived
+const oldest = inventors.sort((a, b) => {
+  const lastGuy = a.passed - a.year;
+  const nextGuy = b.passed - b.year;
+  return lastGuy > nextGuy ? -1 : 1;
+});
+
+// 6. Create a list of Boulevards in Paris that contain 'de' anywhere in the name
+// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+const category = document.querySelector('.mw-category');
+const links = [...category.querySelectorAll('a')]; // [...] converts nodeList to array so you can call .map function
+
+const de = links
+            .map(link => link.textContent);
+            .filter(streetName => streetName.includes('de'));
+
+// 7. Sort people alphabetically by last name
+const alpha = people.sort((lastOne, nextOne) => {
+  const [aLast, aFirst] = lastOne.split(', ');
+  const [bLast, bFirst] = nextOne.split(', ');
+  return aLast > bLast ? 1 : -1;
+});
+
+
+// 8. Reduce exercise
+// Sum up the instances of each of these
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'van', 'car', 'truck'];
+
+const transportation = data.reduce((obj, item) => {
+  if(!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+// start with a blank object with {} and then every time we loop over one, we first see if there is even a zero there to work with at all? If not, we need to make an entry for that and then we increment it.
